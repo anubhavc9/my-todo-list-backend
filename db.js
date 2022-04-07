b25da086e5735712f62e044ccefec8e78bf9497f
anubhavc9 <anubhavc9@github.com>
2022-04-07 18:32:55 +0530

    added connection string

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Pool = require('pg').Pool;
 
+// FOR LOCAL
 // const pool = new Pool({
 //     host: 'localhost',
 //     user: 'postgres',
@@ -7,12 +8,18 @@ const Pool = require('pg').Pool;
 //     port: 5432
 // })
 
+// FOR PRODUCTION
+// const pool = new Pool({
+//   host: 'ec2-3-217-251-77.compute-1.amazonaws.com',
+//   user: 'bmlytkfspbkjqi',
+//   database: 'd7maldq3eev143',
+//   password: '36ff4097a58c363831a19948906d52f91fc9ff2e8bd1131f8b1fa9abf150db83',
+//   port: 5432,
+//   ssl: { rejectUnauthorized: false },
+// });
+
 const pool = new Pool({
-  host: 'ec2-3-217-251-77.compute-1.amazonaws.com',
-  user: 'bmlytkfspbkjqi',
-  database: 'd7maldq3eev143',
-  password: '36ff4097a58c363831a19948906d52f91fc9ff2e8bd1131f8b1fa9abf150db83',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
